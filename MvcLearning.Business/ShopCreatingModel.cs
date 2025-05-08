8dde5d9e17017c4a9fad3e6daf2677dfb0fcb843
DavGasparyannn <zadreff45@gmail.com>
2025-05-08 23:03:37 +0400

    TODO :// SHOP

---
+++ MvcLearning.Business/ShopCreatingModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MvcLearning.Business
+{
+    public class ShopCreatingModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+    }
+}
--- MvcLearning.Business/ShopService.cs
+++ MvcLearning.Business/ShopService.cs
@@ -1,7 +1,25 @@
-﻿namespace MvcLearning.Business
+﻿using Microsoft.AspNetCore.Identity;
+using MvcLearning.Data.Entities;
+using MvcLearning.Data.Interfaces;
+
+namespace MvcLearning.Business
 {
     public class ShopService
     {
+        private readonly IShopRepository _shopRepository;
+        private readonly UserManager<User> _userManager;
+        public ShopService(IShopRepository shopRepository, UserManager<User> userManager)
+        {
+            _shopRepository = shopRepository;
+            _userManager = userManager;
+        }
+        /*public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _shopRepository.GetAllProductsAsync();
+        }*/
+        public async Task CreateShop(ShopCreatingModel shopCreatingModel)
+        {
 
+        }
     }
 }
